9ef10e5208dfb962b9a9348ea55d70ef323da702
مــاڪـي <130955195+Maki550@users.noreply.github.com>
2023-06-03 05:28:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['+963953365082', '𝑀𝐴𝐾𝐼🍷', true],
+  ['+201002163636', '𝐻𝐼𝑁𝐴𝑇𝐴❄', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
+global.mods = ['+201002163636','+963953365082' ] 
+global.prems = ['+963953365082', '+963953365082', '+963953365082']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,11 +47,11 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = '〔 𝑌𝑂𝑁𝐴﹝❄️﹞𝐵𝑂𝑇 〕'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
+global.packname = '𝑀𝐴𝐾𝐼🍷' 
+global.author = '@𝐻𝐼𝑁𝐴𝑇𝐴❄' 
+global.igfg = '▢ Follow on Instagram\nhttps:𝑆𝐴𝑅𝐴𝐻_𝑀𝑁_9' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
 global.fgyt = 'https://youtube.com/@Asliguru'
@@ -59,11 +59,11 @@ global.fgpyp = 'https://youtube.com/@Asliguru'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.dbase = 'mongodb+srv://guru:guru@cluster0.qpggl6x.mongodb.net/?retryWrites=true&w=majority' //ADD YOUR MONGODB BY CHANGIMG IT
 
-global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
-global.rwait = '⌛'
-global.dmoji = '🤭'
+global.wait = '*⚙️ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
+global.rwait = '⚙️'
+global.dmoji = '🤨'
 global.done = '✅'
-global.error = '❌' 
+global.error = '❗' 
 global.xmoji = '🔥' 
 
 global.multiplier = 69 
@@ -89,15 +89,15 @@ import axios from 'axios'
 
 global.owner = [
 
-  ['96178965440', '𝐈𝐓𝐀𝐂𝐇𝐈┇👑', true]
+  ['+963953365082', '𝑀𝐴𝐾𝐼🍷', true]
 
  
 
 ] //Number of owners
 
-global.mods = ['96178965440'] 
+global.mods = ['+963953365082'] 
 
-global.prems = ['96178965440']
+global.prems = ['+963953365082']
 
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 
@@ -161,15 +161,15 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 
-global.botname = '『👑┇𝚁𝙰𝙸𝙻𝚈┇🤖┇𝙱𝙾𝚃┇👑』'
+global.botname = '〔 𝑌𝑂𝑁𝐴﹝❄️﹞𝐵𝑂𝑇 〕'
 
 global.premium = 'true'
 
-global.packname = '𝐈𝐓𝐀𝐂𝐇𝐈┇👑' 
+global.packname = '𝑀𝐴𝐾𝐼🍷' 
 
-global.author = '@Asliguru' 
+global.author = '@𝐻𝐼𝑁𝐴𝑇𝐴❄' 
 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/raily.bot.itachi\n' 
+global.igfg = '▢ Follow on Instagram\nhttps:𝑆𝐴𝑅𝐴𝐻_𝑀𝑁_9' 
 
 global.dygp = 'https://chat.whatsapp.com/BzJhdxwVlte1unMYgieoYQ'
 
@@ -183,17 +183,17 @@ global.fglog = 'https://chat.whatsapp.com/BzJhdxwVlte1unMYgieoYQ'
 
 global.dbase = 'mongodb+srv://guru:guru@cluster0.qpggl6x.mongodb.net/?retryWrites=true&w=majority' //ADD YOUR MONGODB BY CHANGIMG IT
 
-global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
+global.wait = '*⚙️ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
 
-global.rwait = '⌛'
+global.rwait = '⚙️'
 
-global.dmoji = '🤭'
+global.dmoji = '🤨'
 
 global.done = '✅'
 
-global.error = '❌' 
+global.error = '❗' 
 
-global.xmoji = '🔥' 
+global.xmoji = '🙈' 
 
 global.multiplier = 69 
 
